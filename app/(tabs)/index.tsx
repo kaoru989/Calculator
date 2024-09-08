2d6dcfec80ad06723bae5b6e26978847a38d0caf
kaoru98 <kaoruhaha2@gmail.com>
2024-09-08 11:49:15 +0700

    Update index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,168 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import React , {useState} from 'react'
+import { StyleSheet, Text, View, TouchableOpacity, Vibration} from 'react-native';
+import {Entypo} from '@expo/vector-icons';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
 
-export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+export default function App() {
+
+  const [darkMode, setDarkMode] = useState(false);
+  const [currentNumber, setCurrentNumer] = useState('');
+  const [lastNumber, setLastNumer] = useState('');
+  const buttons = ['C', 'DEL', '/', '7', '8', '9', '*', '4', '5', '6', '-', '1', '2','3', '+', '0', '.', '=']
+  function calculator(){
+    let lastArr = currentNumber[currentNumber.length -1]
+    if (lastArr === '/' || 
+      lastArr === '*' || 
+      lastArr === '-' || 
+      lastArr === '+' || 
+      lastArr === '.'){
+      setCurrentNumer(currentNumber)
+      return
+    }
+    else{
+      let result = eval(currentNumber).toString();
+      setCurrentNumer(result)
+      return
+    }
+
+  }
+  function handleInput(buttonPressed){
+    if (buttonPressed === '+' ||
+    buttonPressed === '-' ||
+    buttonPressed === '*' ||
+    buttonPressed === '/' 
+    ){
+      Vibration.vibrate(35);
+      setCurrentNumer(currentNumber+buttonPressed)
+      return
+    }
+    else if (buttonPressed === '1' ||
+    buttonPressed === '2' ||
+    buttonPressed === '3' ||
+    buttonPressed === '4' ||
+    buttonPressed === '5' ||
+    buttonPressed === '6' ||
+    buttonPressed === '7' ||
+    buttonPressed === '8' ||
+    buttonPressed === '9' ||
+    buttonPressed === '0' ||
+    buttonPressed === '-' ){
+      Vibration.vibrate(35);
+    }
+    switch (buttonPressed){
+      case 'DEL':
+        Vibration.vibrate(35);
+        setCurrentNumer(currentNumber.substring(0, (currentNumber.length -1)))
+        return
+      case 'C':
+        Vibration.vibrate(35);
+        setLastNumer('')
+        setCurrentNumer('')
+        return
+      case '=':
+        Vibration.vibrate(35);
+        setLastNumer(currentNumber + '=')
+        calculator()
+        return
+    }
+    setCurrentNumer(currentNumber + buttonPressed)
+  }
+  return(
+    <View>
+      <View style={styles.results}>
+        <TouchableOpacity style={styles.themeButton}>
+          <Entypo name={darkMode ? 'light-up': 'moon'} size={24} color={darkMode ? 'white' : 'black'} 
+            onPress={() => darkMode ? setDarkMode(false) : setDarkMode(true)}/>
+
+        </TouchableOpacity>
+        <Text style={styles.historyText}>{lastNumber}</Text>
+        <Text style={styles.resultsText}>{currentNumber}</Text>
+      </View>
+      <View style={styles.buttons}>
+        {buttons.map((button) =>
+          button === '=' ||
+          button === '/' ||
+          button === '*' ||
+          button === '-' ||
+          button === '+' ?
+          <TouchableOpacity key={button}
+            style={[styles.button, {backgroundColor:'#00b9d6'}]}
+            onPress={()=> handleInput(button)}
+            >
+              <Text style={[styles.textButton, {color:'white', fontSize:28}]}>{button}</Text>
+          </TouchableOpacity>
+          :
+          button === 'C' ?
+            <TouchableOpacity key={button}
+            style={[styles.button, {backgroundColor:typeof(button) === 'number' ?
+            darkMode ? '#303946' : '#fff' : darkMode === true ? '#414853': '#ededed', minWidth: '36%'}]}
+            onPress={()=> handleInput(button)}
+            >
+              <Text style={styles.textButton}>{button}</Text>
+            </TouchableOpacity>
+            :
+            <TouchableOpacity key={button}
+            style={[styles.button, {backgroundColor:typeof(button) === 'number' ?
+            darkMode ? '#303946' : '#fff' : darkMode === true ? '#414853': '#ededed'}]}
+            onPress={()=> handleInput(button)}
+            >
+              <Text style={styles.textButton}>{button}</Text>
+            </TouchableOpacity>
+
+        )} 
+      </View>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
+  results: {
+    backgroundColor: false ? '#282f3b' : '#f5f5f5',
+    maxWidth: '100%',
+    minHeight: '35%',
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end',
+  },
+  resultsText: {
+    maxHeight: 45,
+    color: '#00b9d6',
+    margin: 15,
+    fontSize: 35,
+  },
+  historyText: {
+    color: false ? '#B5B7BB' : '#7c7c7c',
+    fontSize: 20,
+    marginRight: 10,
+    alignSelf: 'flex-end',
+  },
+  themeButton: {
+    alignSelf: 'flex-start',
+    bottom: '5%',
+    margin: 15,
+    backgroundColor: false ? '#7b8084' : '#e5e5e5',
     alignItems: 'center',
-    gap: 8,
+    justifyContent: 'center',
+    width: 50,
+    height: 50,
+    borderRadius: 25,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  buttons: {
+    width: '100%',
+    height: '35%',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  button: {
+    borderColor: false ? '#3f4d5b' : '#e5e5e5',
+    alignItems: 'center',
+    justifyContent: 'center',
+    minWidth: '24%',
+    minHeight: '54%',
+    flex: 2,
   },
-});
+  textButton: {
+    color: false ? '#b5b7bb' : '#7c7c7c',
+    fontSize: 28,
+  }
+})
